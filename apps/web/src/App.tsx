61a322f39fba25d3fa59b4c14ac54c1d0b0bbc00
Patryk Z. <znamirowskipatryk@gmail.com>
2025-11-08 17:23:39 +0100

    Feat: Add auth pages, add site icon, set dark theme only
    # Conflicts:
    #	apps/web/src/app/modules/login/Login.page.tsx
    #	apps/web/src/app/modules/logout/Logout.page.tsx
    #	apps/web/src/app/modules/register/Register.page.tsx

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -4,19 +4,23 @@ import { LoginPage } from "@/app/modules/login/Login.page.tsx";
 import { LogoutPage } from "@/app/modules/logout/Logout.page.tsx";
 import { NotFoundPage } from "@/app/modules/notFound/NotFound.page.tsx";
 import { RegisterPage } from "@/app/modules/register/Register.page.tsx";
+import { Toaster } from "@/components/ui/sonner.tsx";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Navigate to="/home" />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/logout" element={<LogoutPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Toaster position="top-right" />
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate to="/home" />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/logout" element={<LogoutPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
--- apps/web/src/app/modules/logout/Logout.page.tsx
+++ apps/web/src/app/modules/logout/Logout.page.tsx
@@ -1,3 +1,24 @@
+import { Card, CardContent, CardHeader } from "@/components/ui/card.tsx";
+import { Avatar, AvatarImage } from "@/components/ui/avatar.tsx";
+import { FieldLegend, FieldSet } from "@/components/ui/field.tsx";
+
 export const LogoutPage = () => {
-  return "logout";
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <Card>
+        <CardHeader className="justify-center">
+          <Avatar className="size-16 aspect-square">
+            <AvatarImage src="/logo.png" />
+          </Avatar>
+        </CardHeader>
+        <CardContent>
+          <FieldSet>
+            <FieldLegend className="w-full text-center animate-pulse">
+              Logging out...
+            </FieldLegend>
+          </FieldSet>
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
